887507b9b9abe613fbb2d3198b2f9f5a68494935
Islam Mesabah <islamkaloop@gmail.com>
2021-03-24 11:03:22 +0200

    round 1

--- Agent.py
+++ Agent.py
@@ -1,6 +1,69 @@
 import random
+from sklearn.linear_model import Perceptron
+import pickle
+import numpy as np
+import pandas as pd
+import copy
 
-
+import sumo_utils as su
+ 
 class Agent:
+    def __init__(self):
+        self.t = 0
+        self.prev_action = 0
+        self.pref_total_time = 0
+
+    def cost(self,state,conn, vehicle_ids):
+        curr_open_dir = su.get_curr_open_dir(conn)
+        waiting_count = su.get_waiting_count(conn,vehicle_ids)
+        total_waiting_time = su.get_total_waiting_time(conn,vehicle_ids)
+        total_co2 = su.get_total_co2(conn,vehicle_ids)
+        total_accumulated_waiting_time = su.get_total_accumulated_waiting_time(conn,vehicle_ids)
+        total_speed = su.get_total_speed(conn,vehicle_ids)
+        moving_count = su.get_moving_count(conn,vehicle_ids)
+        self.t = self.t+1
+
+        action = curr_open_dir[0]
+        if(self.pref_total_time != 0):
+            if(total_waiting_time > self.pref_total_time):
+                if(self.t>4):
+                    self.t=0
+                    action = (self.prev_action+1)%2
+            else:
+                action = self.prev_action
+
+        
+        self.pref_total_time = total_waiting_time
+        self.prev_action = action
+        
+        # print(self.t)
+        # if(moving_count < waiting_count):
+        #     if(self.t>4):
+        #         self.t=0
+        #         return (curr_open_dir[0]+1)%2
+
+        return action
+        
+
     def select_action(self, state, conn=None, vehicle_ids=None):
-        return random.randint(0, 1)
+        columns=['direction', 'sens1', 'sens2','sens3','sens4','sens5','sens6','sens7','sens8','action']
+        try:
+            df = pd.read_csv("dataset/singleIntersectData1.csv")
+        except:
+            df = pd.DataFrame(columns=columns)
+        if(conn) != None:
+            y = self.cost(state,conn,vehicle_ids)
+
+            data = np.array([state+[y]])
+            df2 = pd.DataFrame(data,columns=columns)
+            df = df.append(df2, ignore_index=True)
+            df.to_csv("dataset/singleIntersectData1.csv", index=False)
+            
+            return y
+
+        else:
+            filename = "DecisionTreeClassifier9.sav"
+            clf = pickle.load(open("models/"+filename, 'rb'))
+            x = clf.predict([state])[0]
+            return int(x)
+                    
--- contestant_main.py
+++ contestant_main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pandas as pd
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -14,9 +15,10 @@ import random
 from Agent import Agent
 from sumo_utils import run_episode
 from gen_sim import gen_sim
-
-NUM_EPISODES = 2  # Number of complete simulation runs
-COMPETITION_ROUND = 1  # 1 or 2, depending on which competition round you are in
+train = True 
+train = False 
+NUM_EPISODES = 1 # Number of complete simulation runs
+COMPETITION_ROUND = 1  # 1 or 2+, depending on which competition round you are in
 random.seed(COMPETITION_ROUND)
 
 """
@@ -39,15 +41,81 @@ if __name__ == "__main__":
 
     # sumoBinary = checkBinary('sumo-gui')
 
+
+    x =[19.0 ,11.833333333333334 ,11.608695652173912 ,13.911764705882353 ,15.0 ,14.39622641509434 ,18.174603174603174 ,20.677419354838708 ,9.757575757575758 ,16.363636363636363 ,16.38888888888889 ,12.078947368421053 ,11.233333333333333 ,21.046511627906977 ,17.105263157894736 ,11.121212121212121 ,12.301369863013699 ,16.854166666666668 ,20.134146341463413 ,23.443396226415093 ,24.557377049180328 ,29.65573770491803 ,10.416666666666666 ,11.038461538461538]
+    
     agent = Agent()  # Instantiate your agent object
     waiting_time_per_episode = []  # A list to hold the average waiting time per vehicle returned from every episode
-
+    i = 1
     for e in range(NUM_EPISODES):
         # Generate an episode with the specified probabilities for lanes in the intersection
         # Returns the number of vehicles that will be generated in the episode
+        # T 1
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                    p_west_east=0.3, p_east_west=0.2,
+        #                    p_north_south=0.2, p_south_north=0.1)
+
+        # T 2
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                    p_west_east=0, p_east_west=2,
+        #                    p_north_south=0, p_south_north=2)
+
+        # T 3
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                    p_west_east=1, p_east_west=1,
+        #                    p_north_south=1, p_south_north=1)
+
+        # T 4
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                    p_west_east=0.2, p_east_west=0.4,
+        #                    p_north_south=0.4, p_south_north=0.2)
+
+        
+        # T 5
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                    p_west_east=0.1, p_east_west=0.71,
+        #                    p_north_south=0.21, p_south_north=0.81)
+
+        # T 6
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                     # N = 50,
+        #                    p_west_east=0, p_east_west=5,
+        #                    p_north_south=5, p_south_north=0)
+        
+        # T 7
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                     # N = 50,
+        #                    p_west_east=0, p_east_west=5,
+        #                    p_north_south=5, p_south_north=5)
+        
+        # T 8
         vehicles = gen_sim('', round=COMPETITION_ROUND,
-                           p_west_east=0.3, p_east_west=0.2,
-                           p_north_south=0.2, p_south_north=0.1)
+                            # N = 50,
+                           p_west_east=5, p_east_west=5,
+                           p_north_south=5, p_south_north=0)
+
+        # T 9
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                     # N = 50,
+        #                    p_west_east=5, p_east_west=0,
+        #                    p_north_south=0, p_south_north=5)
+
+        # T 10       
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                     # N = 50,
+        #                    p_west_east=0.1, p_east_west=0,
+        #                    p_north_south=0, p_south_north=0.1)
+
+        # T 11
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                     # N = 50,
+        #                    p_west_east=0, p_east_west=0.1,
+        #                    p_north_south=0.1, p_south_north=0)
+
+        # T 12
+        # vehicles = gen_sim('', round=COMPETITION_ROUND,
+        #                    p_west_east=0.5, p_east_west=0.5,
+        #                    p_north_south=0.5, p_south_north=0.5)
 
         print('Starting Episode ' + str(e) + '...')
 
@@ -60,7 +128,9 @@ if __name__ == "__main__":
         conn = traci.getConnection("contestant")
         # Run a complete simulation episode with the agent taking actions for as long as the episode lasts.
         # An episode lasts as long as there are cars in the simulation AND the time passed < 1000 seconds
-        total_waiting_time, waiting_times, total_emissions = run_episode(conn, agent, COMPETITION_ROUND)
+        total_waiting_time, waiting_times, total_emissions = run_episode(conn, agent, COMPETITION_ROUND , train = train)
+       
+        # total_waiting_time, waiting_times, total_emissions = run_episode(conn, agent, COMPETITION_ROUND)
         # Cleaning up TraCi environments
         traci.switch("contestant")
         traci.close()
@@ -71,3 +141,14 @@ if __name__ == "__main__":
 
         print('episode[' + str(e) + '] Average waiting time = ' + str(avg_waiting_time)
               + ' (s) -- Average Emissions (CO2) = ' + str(avg_emissions) + "(g)")
+
+        if(train):
+            i = i+1
+            if(i>50):
+                df = pd.read_csv("dataset/singleIntersectDataTotalTime.csv")
+                df1 = pd.read_csv("dataset/singleIntersectData1.csv")
+                df = df.append(df1, ignore_index=True)
+                df1.drop(df1.index, inplace=True)
+                df.to_csv("dataset/singleIntersectDataTotalTime.csv", index=False)
+                df1.to_csv("dataset/singleIntersectData1.csv", index=False)
+                i = 0
